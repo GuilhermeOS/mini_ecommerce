617c063cfa828b5943a08f109dd907ac47b77553
GuilhermeOS <guilherme.etg@outlook.com>
2022-08-01 22:17:52 -0300

    att endpoint

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,6 +1,6 @@
 class Constantes {
   static const productBaseUrl =
-      "https://mini-ecommerce-b6674-default-rtdb.firebaseio.com/products";
+      "https://miniecommerce-db5a8-default-rtdb.firebaseio.com/products";
   static const ordersBaseUrl =
-      "https://mini-ecommerce-b6674-default-rtdb.firebaseio.com/orders";
+      "https://miniecommerce-db5a8-default-rtdb.firebaseio.com/orders";
 }
